27d64c7a6358bb926247f4a56b9e5224f09fc90f
12leslie <https://github.com/12leslie/C-/pulls>
2023-05-29 11:32:09 -0600

    0.1

---
+++ Mi proyecto Nomina/Empleados.cs
@@ -0,0 +1,8 @@
+﻿public class Empleado
+{
+    public string Nombre { get; set; }
+    public string Cargo { get; set; }
+    public decimal SalarioBase { get; set; }
+    public int HorasExtras { get; set; }
+    public decimal TotalPagar { get; set; }
+}
